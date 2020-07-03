736c707b089b83c9ed101666134753d2c9404733
Andrew Munroe <andrewemunroe@gmail.com>
2020-07-03 14:10:10 -0700

    Site footer

---
+++ footer.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * The template for displaying the footer
+ *
+ * Contains the closing of the #content div and all content after.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package Global_Reporting_Program
+ */
+
+?>
+
+	<footer id="colophon" class="site-footer">
+		<div class="site-info">
+      <a class="button" href="#">Learn more about the GRP</a>
+      <div>
+        <span>An initiative of the <a href="https://globalreportingcentre.org/">Global Reporting Centre</a></span>
+        <a href="https://globalreportingcentre.org/"><img width="120" src="https://globalreportingcentre.org/wp-content/uploads/2020/06/global_reporting_centre_logo.svg" /></a>
+      </div>
+		</div>
+	</footer>
+</div>
+
+<?php wp_footer(); ?>
+
+</body>
+</html>
