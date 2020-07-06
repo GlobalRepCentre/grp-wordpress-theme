a5315382d1efdcb024e9a98c2c99dda74c163187
Andrew Munroe <andrewemunroe@gmail.com>
2020-07-06 14:42:09 -0700

    Remaining initial theme files

--- functions.php
+++ functions.php
@@ -61,25 +61,10 @@ if ( ! function_exists( 'global_reporting_program_setup' ) ) :
 		add_theme_support(
 			'html5',
 			array(
-				'search-form',
-				'comment-form',
-				'comment-list',
 				'gallery',
 				'caption',
 				'style',
 				'script',
-			)
-		);
-
-		// Set up the WordPress core custom background feature.
-		add_theme_support(
-			'custom-background',
-			apply_filters(
-				'global_reporting_program_custom_background_args',
-				array(
-					'default-color' => 'ffffff',
-					'default-image' => '',
-				)
 			)
 		);
 
@@ -124,41 +109,17 @@ function global_reporting_program_content_width() {
 add_action( 'after_setup_theme', 'global_reporting_program_content_width', 0 );
 
 /**
- * Register widget area.
- *
- * @link https://developer.wordpress.org/themes/functionality/sidebars/#registering-a-sidebar
- */
-function global_reporting_program_widgets_init() {
-	register_sidebar(
-		array(
-			'name'          => esc_html__( 'Sidebar', 'global-reporting-program' ),
-			'id'            => 'sidebar-1',
-			'description'   => esc_html__( 'Add widgets here.', 'global-reporting-program' ),
-			'before_widget' => '<section id="%1$s" class="widget %2$s">',
-			'after_widget'  => '</section>',
-			'before_title'  => '<h2 class="widget-title">',
-			'after_title'   => '</h2>',
-		)
-	);
-}
-add_action( 'widgets_init', 'global_reporting_program_widgets_init' );
-
-/**
  * Enqueue scripts and styles.
  */
 function global_reporting_program_scripts() {
   wp_enqueue_style('font', 'https://fonts.googleapis.com/css2?family=Gothic+A1:wght@500;800&display=swap');
 	wp_enqueue_style( 'global-reporting-program-style', get_stylesheet_uri(), array(), _S_VERSION );
-	wp_style_add_data( 'global-reporting-program-style', 'rtl', 'replace' );
 
 	wp_enqueue_script( 'global-reporting-program-navigation', get_template_directory_uri() . '/js/navigation.js', array(), _S_VERSION, true );
 
-	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
-		wp_enqueue_script( 'comment-reply' );
-  }
-  
   if ( is_front_page() ) {
     wp_enqueue_script( 'global-reporting-program-header-fader', get_template_directory_uri() . '/js/header-fader.js', array(), _S_VERSION, true );
+    wp_enqueue_script( 'font-awesome', 'https://kit.fontawesome.com/8a020944ca.js', array(), _S_VERSION, false );
   }
 }
 add_action( 'wp_enqueue_scripts', 'global_reporting_program_scripts' );
@@ -174,19 +135,6 @@ require get_template_directory() . '/inc/custom-header.php';
 require get_template_directory() . '/inc/template-tags.php';
 
 /**
- * Functions which enhance the theme by hooking into WordPress.
- */
-require get_template_directory() . '/inc/template-functions.php';
-
-/**
  * Customizer additions.
  */
 require get_template_directory() . '/inc/customizer.php';
-
-/**
- * Load Jetpack compatibility file.
- */
-if ( defined( 'JETPACK__VERSION' ) ) {
-	require get_template_directory() . '/inc/jetpack.php';
-}
-
---
+++ inc/template-tags.php
@@ -0,0 +1,96 @@
+<?php
+/**
+ * Custom template tags for this theme
+ *
+ * Eventually, some of the functionality here could be replaced by core features.
+ *
+ * @package Global_Reporting_Program
+ */
+
+if ( ! function_exists( 'global_reporting_program_post_thumbnail' ) ) :
+	/**
+	 * Displays an optional post thumbnail.
+	 *
+	 * Wraps the post thumbnail in an anchor element on index views, or a div
+	 * element when on single views.
+	 */
+	function global_reporting_program_post_thumbnail() {
+		if ( post_password_required() || is_attachment() || ! has_post_thumbnail() ) {
+			return;
+		}
+
+		if ( is_singular() ) :
+      // If we indicated we want to check for a caption
+      if ($caption) : 
+        // and the caption exists
+        if ( get_the_post_thumbnail_caption() ) : ?>
+          <figure>
+            <?php the_post_thumbnail(); ?>
+            <figcaption><?php the_post_thumbnail_caption(); ?></figcaption>
+          </figure>
+        <?php else :
+          the_post_thumbnail();
+        endif;
+
+      else : ?>
+        <div class="img-wrapper">
+          <?php the_post_thumbnail(); ?>
+        </div>
+      
+      <?php endif; ?>
+
+		<?php else : ?>
+
+			<a class="post-thumbnail" href="<?php the_permalink(); ?>" aria-hidden="true" tabindex="-1">
+				<?php
+					the_post_thumbnail(
+						'post-thumbnail',
+						array(
+							'alt' => the_title_attribute(
+								array(
+									'echo' => false,
+								)
+							),
+						)
+					);
+				?>
+			</a>
+
+			<?php
+		endif; // End is_singular().
+	}
+endif;
+
+
+if ( ! function_exists( 'global_reporting_program_post_thumbnail_caption' ) ) :
+  	/**
+	 * Displays a post thumbnail's caption
+	 *
+	 * Wraps the post thumbnail in an anchor element on index views, or a div
+	 * element when on single views.
+	 */
+
+  function global_reporting_program_post_thumbnail_caption() {
+		if ( is_attachment() || ! has_post_thumbnail() ) {
+			return;
+    }
+
+    if ( get_the_post_thumbnail_caption() ) : ?>
+
+      <span><?php the_post_thumbnail_caption(); ?></span>
+  
+    <?php endif;
+  }
+
+endif;
+
+if ( ! function_exists( 'wp_body_open' ) ) :
+	/**
+	 * Shim for sites older than 5.2.
+	 *
+	 * @link https://core.trac.wordpress.org/ticket/12563
+	 */
+	function wp_body_open() {
+		do_action( 'wp_body_open' );
+	}
+endif;
