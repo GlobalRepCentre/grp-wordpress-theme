3b56a14fc85d770a511b1fdc1e2120acbab716a7
Andrew Munroe <andrewemunroe@gmail.com>
2020-09-08 15:16:41 -0700

    Update projects display template

--- template-parts/sub-templates/project-featured.php
+++ template-parts/sub-templates/project-featured.php
@@ -25,11 +25,13 @@ $url = get_field('project_link');
       <div>
         <?php if ( $year ) : ?><span class="year"><?php the_field('project_year'); ?></span><?php endif; ?>
         <?php the_title( '<h2 class="entry-title">', '</h2>' ); ?>
-        <?php if ( have_rows('partner_logos') ) : 
-          while( have_rows('partner_logos') ) : the_row(); 
+        <?php if ( have_rows('partner_logos') ) : ?>
+          <div class="logos-container">
+          <?php while( have_rows('partner_logos') ) : the_row(); 
             $img = get_sub_field('logo'); ?>
             <img class="logo" src="<?php echo esc_url($img['url']); ?>" alt="<?php echo esc_attr($img['alt']); ?>" />
           <?php endwhile; ?>
+          </div>
         <?php endif; ?>
         <a class="button filled" href="<?php if ( $url ) : echo esc_url( $url ); else : the_permalink(); endif; ?>">View project</a>
       </div>
